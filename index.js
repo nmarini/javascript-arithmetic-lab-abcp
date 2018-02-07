1e932a6ac84456f5596414de3bf6b980f291326a
Nicolas Marini <nmarini44@gmail.com>
2018-02-07 22:01:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function add(a,b) {var result = a + b; return result}
+<<<<<<< HEAD
 function subtract(a,b) {var result = a-b; return result}
 function multiply(a,b) {var result = a*b; return result}
 function divide(a,b) {var result = a/b; return result}
@@ -6,3 +7,7 @@ function inc(n) {var result = n+1; return result }
 function dec(n) {var result = n-1; return result}
 function makeInt(n) {var result = parseInt(n,10); return result}
 function preserveDecimal(n) {var result = parseFloat(n); return result}
+=======
+function subtract(a,b) 
+function multiply(a,b) {var results = a * b; return results}
+>>>>>>> 9c8d2229d977794c6353a3fc790b1150379476c1
